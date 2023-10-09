7833c9207a9db74140324d2a5f45b070d2fbb936
Brandon Copeland <47774770+Bcopeland64@users.noreply.github.com>
2023-10-09 20:10:01 +0300

    Add files via upload

---
+++ WK 4/linkdownload.py
@@ -0,0 +1,34 @@
+#Download images from the links provided
+image_links = ['https://www.istockphoto.com/photo/creative-minimal-paper-idea-concept-white-shoe-with-white-background-3d-render-3d-gm1149654373-310923740?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwhite-sneaker&utm_term=white%20sneaker%3A%3A%3A, https://www.istockphoto.com/photo/pair-of-black-leather-woman-shoes-gm618943308-107831673?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblack-heels&utm_term=black%20heels%3A%3A%3A
+https://www.istockphoto.com/photo/running-shoes-gm1249496770-364161908?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblue-sport-shoes&utm_term=blue%20sport%20shoes%3A%3A%3A,
+https://www.istockphoto.com/photo/brown-leather-loafers-on-white-background-gm91365821-3792004?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbrown-loafers&utm_term=brown%20loafers%3A%3A%3A
+https://unsplash.com/photos/a-QH9MAAVNI, 
+https://www.istockphoto.com/photo/black-brogue-gm861939954-142811079?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblack-oxford-shoes&utm_term=black%20oxford%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/ladies-gold-sandals-gm175397140-20343289?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgold-sandals&utm_term=gold%20sandals%3A%3A%3A
+https://www.istockphoto.com/photo/hiking-boots-gm453563727-25939337?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbrown-hiking-boots&utm_term=brown%20hiking%20boots%3A%3A%3A
+https://www.istockphoto.com/photo/leather-shoe-gm1408146643-459099433?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Ftan-dress-shoes&utm_term=tan%20dress%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/high-top-sneakers-gm98141262-1124120?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblack-hightop-sneakers&utm_term=black%20hightop%20sneakers%3A%3A%3A
+https://www.istockphoto.com/photo/blue-sneaker-on-a-white-background-gm182842831-13766533?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblue-canvas-shoes&utm_term=blue%20canvas%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/photo-of-sport-equipment-gm1396109210-450977671?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgrey-running-shoes&utm_term=grey%20running%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/a-bright-red-high-heel-womans-shoe-by-itself-gm465388887-8510996?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fred-heels&utm_term=red%20heels%3A%3A%3A
+https://www.istockphoto.com/photo/shoe-gm185243689-19675809?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbeige-flats-shoes&utm_term=beige%20flats%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/sport-shoes-gm184338694-17350707?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgrey-sneakers&utm_term=grey%20sneakers%3A%3A%3A
+https://www.istockphoto.com/photo/green-flip-flop-gm172788852-6166934?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgreen-flip-flops&utm_term=green%20flip%20flops%3A%3A%3A
+https://www.istockphoto.com/photo/mens-leather-moccasins-on-a-white-background-gm1481397328-508708535?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblack-loafers&utm_term=black%20loafers%3A%3A%3A
+https://www.istockphoto.com/photo/a-pair-of-hiking-shoes-gm1661504770-535027809?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbrown-hiking-shoes&utm_term=brown%20hiking%20shoes%3A%3A%3A
+dress-shoes-black.png
+https://www.istockphoto.com/photo/white-high-top-sneaker-gm877343900-244835232?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwhite-high-tops&utm_term=white%20high%20tops%3A%3A%3A
+https://www.istockphoto.com/photo/sports-shoe-gm628210852-111464157?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fred-canvas-shoes&utm_term=red%20canvas%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/mens-blue-white-black-and-gray-running-sneaker-gm182663495-12387218?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblue-running-shoes&utm_term=blue%20running%20shoes%3A%3A%3A
+https://www.istockphoto.com/photo/high-heels-shoes-gm184967562-18983866?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fsilver-heels&utm_term=silver%20heels%3A%3A%3A
+https://www.istockphoto.com/photo/pair-of-new-trendy-navy-blue-sneakers-with-white-stripes-isolated-on-white-background-gm1607093285-530639997?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fnavy-blue--shoes&utm_term=navy%20blue%20%20shoes%3A%3A%3A
+
+for image_link in image_links:
+    image_name = image_link.split('/')[4]
+    urllib.request.urlretrieve(image_link, image_name)
+    print('Completed Download')
+    print(image_name)
+    
+# Path: WK%204/linkdownload.py
+#Download images from the links provided
+ path = ''
